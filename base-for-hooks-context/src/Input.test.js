50a3f3f143e0d7a13b6fc1ffb50be63ff9deed42
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-09-08 13:51:26 +0200

    Prop type test for input component

--- base-for-hooks-context/src/Input.test.js
+++ base-for-hooks-context/src/Input.test.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { findByTestAttr } from '../test/testUtils'
+import { findByTestAttr, checkProps } from '../test/testUtils'
 import Input from './Input'
 
+const defaultProps = {
+	secretWord: '',
+}
+
 const setup = () => {
-	return shallow(<Input />)
+	return shallow(<Input {...defaultProps} />)
 }
 
 test('Input renders without error', () => {
 	const wrapper = setup()
 	const component = findByTestAttr(wrapper, 'component-input')
 	expect(component.length).toBe(1)
+})
+
+test('does not throw warning with expected props', () => {
+	checkProps(Input, defaultProps)
 })
