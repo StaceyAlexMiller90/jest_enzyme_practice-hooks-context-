eab3cf329c679bf4202eb2f6f48ff2ec6476515e
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-09-10 15:23:20 +0200

    getSecretWord tests passing

--- base-for-hooks-context/src/actions/hookActions.test.js
+++ base-for-hooks-context/src/actions/hookActions.test.js
@@ -1,2 +1,29 @@
 import moxios from 'moxios'
 import { getSecretWord } from './hookActions'
+
+describe('moxios tests', () => {
+	// Tell requests to use moxios instead of axios
+	beforeEach(() => {
+		moxios.install()
+	})
+	afterEach(() => {
+		moxios.uninstall()
+	})
+	test('calls the getSecretWord callback on axios response', async () => {
+		const secretWord = 'party'
+		//Mocking response from axios when random word server is called
+		moxios.wait(() => {
+			const request = moxios.requests.mostRecent()
+			request.respondWith({
+				status: 200,
+				response: secretWord,
+			})
+		})
+		//create mock for callback argument
+		const mockSetSecretWord = jest.fn()
+		await getSecretWord(mockSetSecretWord)
+
+		// See whether mock was run with the correct argument
+		expect(mockSetSecretWord).toHaveBeenCalledWith(secretWord)
+	})
+})
