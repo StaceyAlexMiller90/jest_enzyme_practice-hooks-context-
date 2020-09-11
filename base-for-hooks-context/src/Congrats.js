c163d2c78f69d73686c6ab8a6181e313ef218d77
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-09-11 16:39:13 +0200

    Added language contect to congrats component

--- base-for-hooks-context/src/Congrats.js
+++ base-for-hooks-context/src/Congrats.js
@@ -1,34 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-/**
- * Functional react component for congratulatory message.
- * @function
- * @param {object} props - React props.
- * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
- */
+import languageContext from './contexts/languageContext'
+import stringsModule from './helpers/strings'
+
 const Congrats = (props) => {
-  if (props.success) {
-    return (
-      <div data-test="component-congrats" className="alert alert-success">
-        <span data-test="congrats-message">
-          Congratulations! You guessed the word!
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div data-test="component-congrats" />
-    );
-  }
-};
+	const language = React.useContext(languageContext)
+
+	if (props.success) {
+		return (
+			<div data-test="component-congrats" className="alert alert-success">
+				<span data-test="congrats-message">
+					{stringsModule.getStringByLanguage(language, 'congrats')}
+				</span>
+			</div>
+		)
+	} else {
+		return <div data-test="component-congrats" />
+	}
+}
 
 Congrats.propTypes = {
-  success: PropTypes.bool.isRequired,
-};
+	success: PropTypes.bool.isRequired,
+}
 
-const mapStateToProps = ({success}) => {
-  return {success};
-};
+const mapStateToProps = ({ success }) => {
+	return { success }
+}
 
-export default Congrats;
+export default Congrats
