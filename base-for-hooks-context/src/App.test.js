085c9becd89d83114fc746078ab3251cd2a09b02
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-09-11 11:48:49 +0200

    Code useReducer and useEffect to pass test

--- base-for-hooks-context/src/App.test.js
+++ base-for-hooks-context/src/App.test.js
@@ -1,14 +1,32 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import { findByTestAttr } from '../test/testUtils'
 import App from './App'
+import hookActions from './actions/hookActions'
 
+const mockGetSecretWord = jest.fn()
+
+//useEffect can only use mount not shallow
 const setup = () => {
-	return shallow(<App />)
+	//Clear the mock everytime as later tests will see previous
+	//calls and count them
+	mockGetSecretWord.mockClear()
+	hookActions.getSecretWord = mockGetSecretWord
+	return mount(<App />)
 }
 
 test('App renders without error', () => {
 	const wrapper = setup()
 	const component = findByTestAttr(wrapper, 'component-app')
 	expect(component.length).toBe(1)
+})
+
+describe('getSecretWordCalls', () => {
+	test('getSecretWord is called on app mount', () => {
+		setup()
+		expect(mockGetSecretWord).toHaveBeenCalled()
+	})
+	test('getSecretWord is not called on app update', () => {
+		setup()
+	})
 })
